b241aab8770e8accc111ac6636ec46a8974f9b1e
Ricardo <r.cornejo@unay.pe>
2020-07-12 16:59:02 -0500

    Todas las clases

---
+++ Grupo3/rcornejo/matricula.py
@@ -0,0 +1,5 @@
+class matricula:
+      __log = utils.log("matricula")
+  def __init__(self, idmatricula):
+    self.idmatricula = idmatricula
+    
---
+++ Grupo3/rcornejo/periodoEscolar.py
@@ -0,0 +1,7 @@
+#Clase Periodo Escolar
+class periodoEscolar:
+      __log = utils.log("periodo escolar")
+  def __init__(self, idperiodoEscolar, desperiodo):
+    self.idperiodoEscolar = idperiodoEscolar
+    self.desperiodo = desperiodo
+   
