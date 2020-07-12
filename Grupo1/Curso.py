03b17475f9330333714cc96f2c1d17f286f48f8b
reynerdev <rloza@pucp.pe>
2020-07-11 20:52:54 -0500

    Inicio

---
+++ Grupo1/Curso.py
@@ -0,0 +1,7 @@
+from connection.conn import Connection
+
+
+connection = Connection(
+    'mongodb+srv://reyner:pachaqtec@pachaqtec.sdvq7.mongodb.net/pachaqtec?retryWrites=true&w=majority', 'pachacteq')
+
+print(connection.obtenerRegistro('profesores', None))
---
+++ Grupo1/connection/conn.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient, errors
+from pprint import PrettyPrinter
+
+
+class Connection:
+    def __init__(self, uri, database):
+        self.client = MongoClient(uri)
+        self.db = self.client.get_database('student_db')
+        print("Conexion Exitosa")
+
+    # Metodo insertar un registro
+
+    def insertRegistro(self, collection, data):
+        collection = self.db[collection]
+        result = collection.insert_one(data)
+        print(f"Se ingreso el registro : {result.inserted_id} ")
+
+    # Metodo obtener registros
+    def obtenerRegistros(self, collection, condition={}):
+        collection = self.db[collection]
+        data = collection.find(condition)
+        return list(data)
+
+    # Metodo obtener un solo registro
+    def obtenerRegistro(self, collection, condition={}):
+        collection = self.db[collection]
+        data = collection.find_one(condition)
+        return data
+
+    # Metodo cerrar conexion
+    def cerrarConexion(self):
+        self.db.close()
+        return True
