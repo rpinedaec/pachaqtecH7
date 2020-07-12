21bfa32d7450e3f3ca6a7585cb5e905f4f3a7539
reynerdev <58928047+reynerdev@users.noreply.github.com>
2020-07-11 21:36:35 -0500

    Update Curso.py

--- Grupo1/Curso.py
+++ Grupo1/Curso.py
@@ -1,7 +1,6 @@
 from connection.conn import Connection
 
 
-connection = Connection(
-    'mongodb+srv://reyner:pachaqtec@pachaqtec.sdvq7.mongodb.net/pachaqtec?retryWrites=true&w=majority', 'pachacteq')
+connection = Connection('', 'pachacteq')
 
 print(connection.obtenerRegistro('profesores', None))
