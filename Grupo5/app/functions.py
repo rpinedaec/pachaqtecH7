3a6380bb2a1fa57a78059dd57d5313bb0c1cf1c6
BrucePM <brucekevin45@gmail.com>
2020-07-15 23:21:33 -0500

    final

--- Grupo5/app/functions.py
+++ Grupo5/app/functions.py
@@ -694,6 +694,60 @@ def crearNota():
     colec = my_db["notas"]
     colec.insert_one({"cod_matricula":codmatricula, "cod_curso":codCurso, "nota": nota})
     time.sleep(2)
+def modificarNota():
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    my_db = myclient["Hackaton_S7_G5"]
+    colec = my_db["matriculas"]
+    listAlumnos = colec.find()
+    print("cod_matricula\talumno\t\tnombre\t\tgrado\t\tperiodo")
+    for x in colec.find():
+        alumno=x["alumno"]
+        grado = x["grado"]
+        periodo = x["periodo"]
+        print(str(x["cod_matricula"]) + "\t\t" + alumno["nombre"] + "\t\t" + alumno["apellido"] + "\t" + grado["desc"] + "\t" + periodo["desc"])   
+    codmatricula = int(input("Ingrese el codigo de matricula del alumno a modificar la nota: "))
+    print("cod_curso\tnombre")
+    dicalumno=colec.find_one({"cod_matricula":codmatricula})
+    numcursos=len(dicalumno["cursos"])
+    for i in range(numcursos):
+        cod = dicalumno["cursos"][i][0]["cod_curso"]
+        nombre = dicalumno["cursos"][i][0]["nombre"]
+        print(f'{cod}\t\t{nombre}')
+    codCurso = int(input("Ingrese el codigo de curso: "))
+    nota = int(input("ingrese la nueva nota del cuso: "))
+    colec.update_one({"cod_matricula":codmatricula, "cod_curso":codCurso, "nota": nota})
+    print("cod_matricula\tcod_curso\t\tnota")
+    for x in colec.find({},{"cod_matricula":codmatricula, "cod_curso":codCurso, "nota": nota}):
+        print (str(x["cod_matricula"]) + "\t\t" + x["cod_curso"] + "\t\t" + x["nota"])
+    print("Se modifico correctamente")
+    time.sleep(2)
+def eliminarNota():
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    my_db = myclient["Hackaton_S7_G5"]
+    colec = my_db["matriculas"]
+    listAlumnos = colec.find()
+    print("cod_matricula\talumno\t\tnombre\t\tgrado\t\tperiodo")
+    for x in colec.find():
+        alumno=x["alumno"]
+        grado = x["grado"]
+        periodo = x["periodo"]
+        print(str(x["cod_matricula"]) + "\t\t" + alumno["nombre"] + "\t\t" + alumno["apellido"] + "\t" + grado["desc"] + "\t" + periodo["desc"])   
+    codmatricula = int(input("Ingrese el codigo de matricula del alumno a elimar la nota: "))
+    print("cod_curso\tnombre")
+    dicalumno=colec.find_one({"cod_matricula":codmatricula})
+    numcursos=len(dicalumno["cursos"])
+    for i in range(numcursos):
+        cod = dicalumno["cursos"][i][0]["cod_curso"]
+        nombre = dicalumno["cursos"][i][0]["nombre"]
+        print(f'{cod}\t\t{nombre}')
+    codCurso = int(input("Ingrese el codigo de curso: "))
+    resConn = colec.delete_one({"nota": nota})
+    os.system("cls")
+    print("cod_matricula\tcod_curso\t\tnota")
+    for x in colec.find({},{"cod_matricula":codmatricula, "cod_curso":codCurso, "nota": nota}):
+        print (str(x["cod_matricula"]) + "\t\t" + x["cod_curso"] + "\t\t" + x["nota"])
+    print("Se elimino correctamente")
+    time.sleep(2)
 
 #LISTAR
 def listarAlumnos():
